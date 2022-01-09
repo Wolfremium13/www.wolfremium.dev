6031d236b3f5f7713f942824df4f4a19a49e86ff
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-01-09 11:07:57 +0000

    💄 Update code style

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import "../styles/prism-night-owl.css";
+import "../styles/prism-darcula.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
