a442d449a78b175ec20c9727af1426c81f2df9de
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-01-23 20:47:43 +0000

    ✨Add posts

--- components/Search.js
+++ components/Search.js
@@ -1,65 +1,51 @@
-import { useCallback, useRef, useState } from 'react'
+import { useCallback, useRef, useState } from "react";
 import NextLink from "next/link";
-import { PostListItem } from "./blog/PostCompact";
 import { formatDate } from "../lib/format-date";
 
 export default function Search() {
+  const searchRef = useRef(null);
+  const [query, setQuery] = useState("");
+  const [active, setActive] = useState(false);
+  const [results, setResults] = useState([]);
 
-  const searchRef = useRef(null)
-  const [query, setQuery] = useState('')
-  const [active, setActive] = useState(false)
-  const [results, setResults] = useState([])
-
-  const searchEndpoint = (query) => `/api/search?q=${query}`
+  const searchEndpoint = (query) => `/api/search?q=${query}`;
 
   const onChange = useCallback((event) => {
     const query = event.target.value;
-    setQuery(query)
+    setQuery(query);
     if (query.length) {
       fetch(searchEndpoint(query))
-        .then(res => res.json())
-        .then(res => {
-          setResults(res.results)
-        })
+        .then((res) => res.json())
+        .then((res) => {
+          setResults(res.results);
+        });
     } else {
-      setResults([])
+      setResults([]);
     }
-  }, [])
+  }, []);
 
   const onFocus = useCallback(() => {
-    setActive(true)
-    window.addEventListener('click', onClick)
-  }, [])
+    setActive(true);
+    window.addEventListener("click", onClick);
+  }, []);
 
   const onClick = useCallback((event) => {
     if (searchRef.current && !searchRef.current.contains(event.target)) {
-      setActive(false)
-      window.removeEventListener('click', onClick)
+      setActive(false);
+      window.removeEventListener("click", onClick);
     }
-  }, [])
+  }, []);
 
   return (
-    <div
-      ref={searchRef}
-    >
+    <div ref={searchRef}>
       <input
         onChange={onChange}
         onFocus={onFocus}
-        placeholder='Search posts'
-        type='text'
+        placeholder="Search posts"
+        type="text"
         value={query}
       />
-      { active && results.length > 0 && results.map((post) => (
-          <NextLink href={post.slug} key={post.slug}>
-            <a>
-              <PostListItem
-                title={post.title}
-                date={formatDate(post.date)}
-                tags={post.tags}
-              />
-            </a>
-          </NextLink>
-        ))}
+      {active && results.length > 0 && results.map((post) => <></>)}
     </div>
-  )
+  );
 }
--- pages/blog.js
+++ pages/blog.js
@@ -3,7 +3,7 @@ import { Text, Container } from "@chakra-ui/react";
 import { orderByDate } from "../lib/order-by-date";
 import { getAllFilesMetadata } from "../lib/mdx";
 import { usePagination } from "../lib/use-pagination";
-import PostCompact from "../components/blog/PostCompact";
+import PostList from "../components/blog/PostList"
 import Search from "../components/Search";
 
 export default function Blog({ posts }) {
@@ -48,7 +48,7 @@ export default function Blog({ posts }) {
     <Container maxW={"7xl"} p="12">
       {/*     <Search /> */}
       {currentPosts &&
-        currentPosts.map((post) => <PostCompact key={post.slug} post={post} />)}
+        <PostList posts={currentPosts}></PostList>}
       {currentPage !== maxPage && (
         <Text fontSize="xl" fontWeight="bold" p={6} ref={setElement}>
           🐢 Cargando...
