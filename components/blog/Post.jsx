885cb6c34471d0cc0013482b1b3993fe3e937d2b
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-01-24 22:09:42 +0000

    🐛 Fix tags text

--- components/blog/Post.jsx
+++ components/blog/Post.jsx
@@ -4,8 +4,7 @@ import PostDate from "./PostDate";
 import PostReadingTime from "./PostReadingTime";
 import { formatDate } from "../../lib/format-date";
 
-const defaultImage =
-  "https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=800&q=80";
+const defaultImage = "/assets/default-image.jpg";
 
 const hasPreviewImage = (post) => post.preview ?? defaultImage;
 
--- components/blog/PostTags.jsx
+++ components/blog/PostTags.jsx
@@ -1,15 +1,19 @@
-import { HStack, Tag } from "@chakra-ui/react";
+import { HStack, Tag, Text, Flex } from "@chakra-ui/react";
 
 const PostTags = (props) => {
   return (
     <HStack spacing={2} marginTop={props.marginTop}>
-      {props.tags.map((tag) => {
-        return (
-          <Tag size={"lg"} variant="solid" colorScheme="teal" key={tag}>
-            {tag}
-          </Tag>
-        );
-      })}
+      <Flex wrap={"wrap"}>
+        {props.tags.map((tag) => {
+          return (
+            <Tag m="1" size={"lg"} variant="solid" colorScheme="teal" key={tag}>
+              <Text isTruncated key={tag}>
+                {tag}
+              </Text>
+            </Tag>
+          );
+        })}
+      </Flex>
     </HStack>
   );
 };
