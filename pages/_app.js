9da66ecc9ccbd340fac0cf18e6ac9c44983f443f
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-01-25 19:41:48 +0000

    💄 Added styles for scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "github-markdown-css";
 import "../styles/blog.css";
+import "../styles/scroll.css";
 import "../styles/prism-material-oceanic.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme.js";
