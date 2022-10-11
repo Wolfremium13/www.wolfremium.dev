031a686c1dfd9c4c7a17272ad89995e393233a48
kevin.hierro-ext <kevin.hierro-ext@clarity.ai>
2022-10-11 11:32:57 +0100

    💄 update customize image

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const gulp = require('gulp');
 const webp = require('gulp-webp');
 
 //Supports PNG, JPEG, TIFF, WebP
-const BLOG_IMAGES_SRC = 'public/assets/blog/**/*.{jpg,png,tiff,gif,jfif,webp}'
-const IMAGES_SRC = 'public/assets/*.{jpg,png,tiff,gif,jfif,webp}'
+const BLOG_IMAGES_SRC = 'public/assets/blog/**/*.{jpg,png,tiff}'
+const IMAGES_DST = 'public/assets/blog'
 
 gulp.task('default', () =>
     gulp.src(BLOG_IMAGES_SRC)
         .pipe(webp())
-        .pipe(gulp.dest('public/assets/temp'))
+        .pipe(gulp.dest(IMAGES_DST))
 );
