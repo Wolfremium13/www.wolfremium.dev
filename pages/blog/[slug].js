e08899057d23b856ba3b9670af3c2873fc29ac43
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-02-12 16:21:49 +0000

    ✨ added h1 as tittle of markdown

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,26 +1,27 @@
 import { getFileBySlug } from "../../lib/mdx";
 import { MDXRemote } from "next-mdx-remote";
 import { MDXComponents } from "../../components/mdx/MDXComponents";
-import { Heading } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 import { formatDate } from "../../lib/format-date";
 import { getMdxPaths } from "next-mdx/server";
 
 export default function Post({ post }) {
-  const metadata = post.frontMatter
+  const metadata = post.frontMatter;
   return (
     <>
-      <Heading mt="4" align="center">
-        📅 {formatDate(metadata.date)}
+      <Heading align="center" mt={"4"}>
+        <Text color="gray.300">📅 {formatDate(metadata.date)}</Text>
       </Heading>
-      <section className="post-body">
+      <Box className="post-body">
         <article className="markdown-body">
+          <h1 align="center">{metadata.title}</h1>
           <MDXRemote
             {...post.serialize}
             components={MDXComponents}
             metadata={metadata}
           />
         </article>
-      </section>
+      </Box>
     </>
   );
 }
