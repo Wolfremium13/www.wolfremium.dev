bd04362438c244545a2c810f06df229facb58fc3
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-02-20 21:32:58 +0000

    💄 added github button

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,17 @@ import "../styles/markdown.css";
 import "../styles/blog.css";
 import "../styles/scroll.css";
 import "../styles/prism-material-oceanic.css";
+import "../styles/github-corner.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme.js";
 import Layout from "../components/page/Layout";
+import GitHubCorner from "../components/page/GitHubCorner";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <ChakraProvider theme={theme}>
+        <GitHubCorner />
         <Layout>
           <Component {...pageProps} />
         </Layout>
