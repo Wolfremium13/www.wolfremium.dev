3be29e8eb2f7284c22a94fba5fee57217de7c116
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-08-04 22:21:07 +0100

    🔧 extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        darkGreen: "#3f6d4e",
+        lightGreen: "#8bd450",
+        darkViolet: "#1d1a2f",
+        mediumViolet: "#965fd4",
+        lightViolet: "#734f9a",
+      },
+    },
+  },
   plugins: [],
-}
+};
