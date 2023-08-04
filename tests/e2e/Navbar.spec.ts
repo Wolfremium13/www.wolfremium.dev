0597a34b20ecf8dd2dc458206e97a53d0f14efc4
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-08-04 20:18:06 +0100

    ✅  navbar e2e tests

--- tests/e2e/Navbar.spec.ts
+++ tests/e2e/Navbar.spec.ts
@@ -2,23 +2,60 @@ import { test, expect } from '@playwright/test';
 
 
 test.describe('Navigation bar should ', () => {
+  const minimumDesktopWidth = 768;
+  const irrelevantHeight = 120;
 
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3000');
   });
 
-  test('hide and display the navigation links on mobile version', async ({ page }) => {
-    await page.setViewportSize({ width: 500, height: 800 });
-
-    const navLinksHidden = page.getByTestId('navbar-link-container')
-    expect(navLinksHidden).toBeHidden();
-
-    await page.getByTestId('navbar-toggle-button').click();
-    
-    const navLinksVisible = page.getByTestId('navbar-link-container')
-    expect(navLinksVisible).toBeVisible();
+  test.afterEach(async ({ page }) => {
+    await page.close();
   });
 
-})
+  test.describe("on mobile window size", () => {
+    const mobileWidth = minimumDesktopWidth - 1;
+    test("have a toggle button", async ({ page }) => {
+      await page.setViewportSize({ width: mobileWidth, height: irrelevantHeight });
+
+      const button = await page.$('button');
+
+      expect(button).not.toBeNull();
+    });
+
+    test("not display navigation links by default", async ({ page }) => {
+      await page.setViewportSize({ width: mobileWidth, height: irrelevantHeight });
+
+      const links = await page.$$('li');
+
+      expect(links.length).toBe(0);
+    });
+
+    test("display navigation links when button is pressed", async ({ page }) => {
+      await page.setViewportSize({ width: mobileWidth, height: irrelevantHeight });
+
+      await page.click('button');
+
+      const links = await page.$$('li');
+      expect(links.length).toBeGreaterThan(0);
+    });
+  });
+
+  test.describe("on desktop window size", () => {
+    test("not have a toggle button", async ({ page }) => {
+      await page.setViewportSize({ width: minimumDesktopWidth, height: irrelevantHeight });
+      const button = await page.$('button');
+
+      expect(button).toBeNull();
+    });
+
+    test("display navigation links", async ({ page }) => {
+      await page.setViewportSize({ width: minimumDesktopWidth, height: irrelevantHeight });
+
+      const links = await page.$$('li');
+      expect(links.length).toBeGreaterThan(1);
+    });
+  })
+});
 
 
