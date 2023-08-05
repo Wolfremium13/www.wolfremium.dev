1d01b923e1d82d9b8f2c3170d8bc37b34431531e
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-08-05 10:39:55 +0100

    ✅  navbar mock mutation of state

--- tests/unit/Navbar.spec.tsx
+++ tests/unit/Navbar.spec.tsx
@@ -21,13 +21,14 @@ describe("Navigation bar should ", () => {
     });
 
     test("display navigation links when button is pressed", () => {
+      // I cannot mutate the state of the parent component from here
+      const setIsOpenMock = jest.fn(() => {});
       render(
-        <NavbarLogic isMobile={true} isOpen={true} setIsOpen={() => {}} />
+        <NavbarLogic isMobile={true} isOpen={false} setIsOpen={setIsOpenMock} />
       );
       fireEvent.click(screen.getByRole("button"));
 
-      const navigationLinks = screen.queryAllByRole("listitem");
-      expect(navigationLinks.length).toBeGreaterThanOrEqual(1);
+      expect(setIsOpenMock).toHaveBeenCalledTimes(1);
     });
   });
 
