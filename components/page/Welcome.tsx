4c2e0bbf25d428e9e2cc8a950cd7b2b04f8bb482
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-08-05 14:31:17 +0100

    💬 update welcome introduction

--- components/page/Welcome.tsx
+++ components/page/Welcome.tsx
@@ -7,26 +7,30 @@ const Welcome = () => {
     <Card>
       <section className="flex flex-col xl:flex-row items-center justify-center gap-8 p-8">
         <div className="w-full xl:w-1/2 max-w-xl">
-          <Image src={profilePic} alt="Kevin Hierro" className="rounded-full border-4 border-darkGreen" />
+          <Image
+            src={profilePic}
+            alt="Kevin Hierro"
+            className="rounded-full p-1 bg-gradient-to-tr from-lightGreen  to-mediumViolet"
+          />
         </div>
         <div className="w-full xl:w-1/2 text-center xl:text-left">
           <h1 className="text-2xl md:text-4xl font-bold mb-4">
             ¡Hola! Soy Kevin Hierro, pero puedes llamarme Wolfremium.
           </h1>
           <p className="text-lg mb-4">
-            Soy un apasionado desarrollador de software con una amplia
-            experiencia en la creación de aplicaciones web y móviles. Mi amor
-            por la tecnología y la programación me ha llevado a adquirir
-            habilidades en una variedad de lenguajes y tecnologías, incluyendo
-            Python, TypeScript, Java, React y React Native.
+            Soy un artesano del software con experiencia en la creación de
+            aplicaciones web e ingeniería de datos. Mi amor por la tecnología y
+            la programación me ha llevado a adquirir habilidades en una variedad
+            de lenguajes y tecnologías, incluyendo Python, TypeScript, Java,
+            React y Scala.
           </p>
           <p className="text-lg mb-4">
-            A lo largo de mi carrera, he tenido la oportunidad de trabajar con
-            Firebase y AWS, lo que me ha permitido desarrollar soluciones
+            A lo largo de mi carrera, he tenido la oportunidad de asistir a
+            multitud de eventos, lo que me ha permitido desarrollar soluciones
             eficientes y escalables. Mi curiosidad insaciable me ha llevado a
             explorar el fascinante mundo de la inteligencia artificial y el
-            aprendizaje automático, áreas en las que estoy ansioso por
-            profundizar y expandir mis conocimientos.
+            Agile Testing, áreas en las que estoy ansioso por profundizar y
+            expandir mis conocimientos.
           </p>
           <p className="text-lg mb-4">
             En mi tiempo libre, disfruto contribuyendo a la comunidad de código
