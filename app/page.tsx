756e02e8d4b26afc4c052b4f3672414bda357ddd
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-08-05 14:13:53 +0100

    ✨  added  main page style and content

--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,10 @@
 import { Card } from "@/components/page/Card";
+import { Welcome } from "@/components/page/Welcome";
 
 export default function Home() {
   return (
-    <Card>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ex
-        asperiores incidunt! Accusamus repellat cumque atque aperiam eveniet
-        optio similique, necessitatibus voluptate saepe fugiat nihil laudantium,
-        totam minima quae. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Molestiae, corporis quasi natus facere eligendi deleniti cum rem
-        fugiat quibusdam distinctio ea quidem expedita, molestias vel voluptatem
-        alias recusandae perferendis quis. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Sed ex asperiores incidunt! Accusamus repellat cumque atque aperiam
-        eveniet optio similique, necessitatibus voluptate saepe fugiat nihil
-        laudantium, totam minima quae. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Molestiae, corporis quasi natus facere eligendi
-        deleniti cum rem fugiat quibusdam distinctio ea quidem expedita,
-        molestias vel voluptatem alias recusandae perferendis quis. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
-        Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.
-        Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.
-        Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.
-        Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.
-        Quisquam, voluptatum. Lorem ipsum dolor sit, amet consectetur
-        adipisicing elit. Sed ex asperiores incidunt! Accusamus repellat cumque
-        atque aperiam eveniet optio similique, necessitatibus voluptate saepe
-        fugiat nihil laudantium, totam minima quae. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Molestiae, corporis quasi natus facere
-        eligendi deleniti cum rem fugiat quibusdam distinctio ea quidem
-        expedita, molestias vel voluptatem alias recusandae perferendis quis.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Lorem ipsum dolor sit, amet
-        consectetur adipisicing elit. Sed ex asperiores incidunt! Accusamus
-        repellat cumque atque aperiam eveniet optio similique, necessitatibus
-        voluptate saepe fugiat nihil laudantium, totam minima quae. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Molestiae, corporis quasi
-        natus facere eligendi deleniti cum rem fugiat quibusdam distinctio ea
-        quidem expedita, molestias vel voluptatem alias recusandae perferendis
-        quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Lorem ipsum dolor sit, amet
-        consectetur adipisicing elit. Sed ex asperiores incidunt! Accusamus
-        repellat cumque atque aperiam eveniet optio similique, necessitatibus
-        voluptate saepe fugiat nihil laudantium, totam minima quae. Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Molestiae, corporis quasi
-        natus facere eligendi deleniti cum rem fugiat quibusdam distinctio ea
-        quidem expedita, molestias vel voluptatem alias recusandae perferendis
-        quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
-        voluptatum. Quisquam, voluptatum.
-      </p>
-    </Card>
+    <>
+      <Welcome />
+    </>
   );
 }
---
+++ components/page/Welcome.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import { Card } from "./Card";
+import profilePic from "@/public/layout/personal-image.webp";
+
+const Welcome = () => {
+  return (
+    <Card>
+      <section className="flex flex-col xl:flex-row items-center justify-center gap-8 p-8">
+        <div className="w-full xl:w-1/2 max-w-xl">
+          <Image src={profilePic} alt="Kevin Hierro" className="rounded-full border-4 border-darkGreen" />
+        </div>
+        <div className="w-full xl:w-1/2 text-center xl:text-left">
+          <h1 className="text-2xl md:text-4xl font-bold mb-4">
+            ¡Hola! Soy Kevin Hierro, pero puedes llamarme Wolfremium.
+          </h1>
+          <p className="text-lg mb-4">
+            Soy un apasionado desarrollador de software con una amplia
+            experiencia en la creación de aplicaciones web y móviles. Mi amor
+            por la tecnología y la programación me ha llevado a adquirir
+            habilidades en una variedad de lenguajes y tecnologías, incluyendo
+            Python, TypeScript, Java, React y React Native.
+          </p>
+          <p className="text-lg mb-4">
+            A lo largo de mi carrera, he tenido la oportunidad de trabajar con
+            Firebase y AWS, lo que me ha permitido desarrollar soluciones
+            eficientes y escalables. Mi curiosidad insaciable me ha llevado a
+            explorar el fascinante mundo de la inteligencia artificial y el
+            aprendizaje automático, áreas en las que estoy ansioso por
+            profundizar y expandir mis conocimientos.
+          </p>
+          <p className="text-lg mb-4">
+            En mi tiempo libre, disfruto contribuyendo a la comunidad de código
+            abierto. Puedes echar un vistazo a algunos de mis proyectos en mi{" "}
+            <a
+              href="https://github.com/Wolfremium13"
+              className="text-blue-500 underline"
+            >
+              perfil de Github
+            </a>
+            . Cada proyecto que emprendo es un viaje de aprendizaje y una
+            oportunidad para mejorar y perfeccionar mis habilidades.
+          </p>
+          <p className="text-lg mb-4">
+            Actualmente, tengo el placer de ser parte del equipo de Leanmind,
+            donde trabajo con un grupo de personas increíblemente talentosas y
+            apasionadas por lo que hacen.
+          </p>
+          <p className="text-lg">
+            Si tienes alguna pregunta, idea de proyecto o simplemente quieres
+            charlar sobre tecnología, ¡no dudes en ponerte en contacto conmigo!
+          </p>
+        </div>
+      </section>
+    </Card>
+  );
+};
+
+export { Welcome };
