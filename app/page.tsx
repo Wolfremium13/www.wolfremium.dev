2827d989cea0677bf5feb6fa33fd41a33669ec24
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-08-05 11:03:50 +0100

    💄 changed distribution of main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="pt-1 bg-darkViolet text-cyan-50">
+    <section className="">
       <h1>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ex
         asperiores incidunt! Accusamus repellat cumque atque aperiam eveniet
@@ -15,7 +15,59 @@ export default function Home() {
         voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
         voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
         voluptatum.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ex
+        asperiores incidunt! Accusamus repellat cumque atque aperiam eveniet
+        optio similique, necessitatibus voluptate saepe fugiat nihil laudantium,
+        totam minima quae. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae, corporis quasi natus facere eligendi deleniti cum rem
+        fugiat quibusdam distinctio ea quidem expedita, molestias vel voluptatem
+        alias recusandae perferendis quis. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ex
+        asperiores incidunt! Accusamus repellat cumque atque aperiam eveniet
+        optio similique, necessitatibus voluptate saepe fugiat nihil laudantium,
+        totam minima quae. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae, corporis quasi natus facere eligendi deleniti cum rem
+        fugiat quibusdam distinctio ea quidem expedita, molestias vel voluptatem
+        alias recusandae perferendis quis. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ex
+        asperiores incidunt! Accusamus repellat cumque atque aperiam eveniet
+        optio similique, necessitatibus voluptate saepe fugiat nihil laudantium,
+        totam minima quae. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae, corporis quasi natus facere eligendi deleniti cum rem
+        fugiat quibusdam distinctio ea quidem expedita, molestias vel voluptatem
+        alias recusandae perferendis quis. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed ex
+        asperiores incidunt! Accusamus repellat cumque atque aperiam eveniet
+        optio similique, necessitatibus voluptate saepe fugiat nihil laudantium,
+        totam minima quae. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Molestiae, corporis quasi natus facere eligendi deleniti cum rem
+        fugiat quibusdam distinctio ea quidem expedita, molestias vel voluptatem
+        alias recusandae perferendis quis. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam,
+        voluptatum.
       </h1>
-    </main>
+    </section>
   );
 }
