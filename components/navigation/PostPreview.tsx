0b0c9a511a574f75be5ec010f5dce2836971f6f4
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-08-06 16:43:28 +0100

    💄 fix heigh for post preview

--- components/navigation/PostPreview.tsx
+++ components/navigation/PostPreview.tsx
@@ -10,7 +10,7 @@ const PostPreview: React.FC<PostPreviewProps> = ({ title, slug, preview }) => {
   return (
     <div className="border-2 border-darkGreen hover:border-lightGreen rounded-lg overflow-hidden hover:scale-105 transform transition-transform">
       <Link href={`/blog/posts/${slug}`}>
-        <img src={preview} alt={title} className="w-full h-full object-cover" />
+        <img src={preview} alt={title} className="w-full h-80 object-cover" />
         <div className="p-4 absolute bottom-0 bg-gray-900/60 w-full">
           <h2 className="text-xl font-semibold">{title}</h2>
           <p className="text-sm text-gray-300">Leer más</p>
