476a36b973dceedc16c07e53f2956d0c45079f2b
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-08-20 13:50:20 +0100

    💩 added gulp to convert to webp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const webp = require('gulp-webp');
+
+//Supports PNG, JPEG, TIFF, WebP
+const BLOG_IMAGES_SRC = 'public/assets/blog/**/*.{jpg,png,tiff}'
+const IMAGES_DST = 'public/assets/blog'
+
+// TODO: Se if we can use this to convert images to webp using ts instead of gulp
+gulp.task('default', () =>
+    gulp.src(BLOG_IMAGES_SRC)
+        .pipe(webp())
+        .pipe(gulp.dest(IMAGES_DST))
+);
