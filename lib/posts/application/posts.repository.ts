5a0742fa4062f1b2bbd6d2d3f5bcfc431c8a98bb
Wolfremium13 <wolfremiuminformatica@gmail.com>
2024-11-05 16:44:39 +0000

    🚧 move blog in order to prepare the new app routes

--- lib/posts/application/posts.repository.ts
+++ lib/posts/application/posts.repository.ts
@@ -1,7 +1,4 @@
 import {Post} from "@/lib/posts/domain/post";
-
-export type PostFileParams = {locale: string, postName: string}
-
 export interface PostsRepository {
     getTotalPages(): Promise<number>;
     getPaginatedPosts(pageNumber: number): Promise<Post[]>;
