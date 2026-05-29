2371398b5d9b7786f4a62e5b60d71149e57c441d
Wolfremium13 <wolfremiuminformatica@gmail.com>
2026-05-29 20:00:41 +0100

    feat: Add sitemap integration and update layout for SEO enhancements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.wolfremium.dev',
+
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
-  }
+  },
+
+  integrations: [sitemap()]
 });
